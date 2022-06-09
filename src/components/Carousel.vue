4460bfd45bb3a7b8be9237475fc715199b38e356
unknown <mikeman@inbox.ru>
2022-06-09 18:35:57 +0300

    added styles for vue-slick-slider

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,42 +1,79 @@
 <template>
   <div class="carousel">
-    <CarouselItem
-      v-for="item in imagesList" 
-      :key="item.id"
-      :imageItem="item" 
-    />
-    <button type="button" @click="nextSlide">NEXT SLIDE</button>
-    <button type="button" @click="prevSlide">PREV SLIDE</button>
-
+    <div class="slider-wrapper">
+      <button class="prev-btn" @click="showPrev()"></button>
+        <vue-slick-carousel
+          ref="carousel"
+          :centerMode="true"
+          :arrows="true"
+          :infinite="true"
+          :slides-to-show="4"
+          :dots="false"
+          class="carousel"
+          :style="{'display': flex, 'width': '1500px', 'align-items': 'center'}"
+        >
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/card.png')">
+            <img v-else :src="require('@/assets/images/card-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/umbrella.png')">
+            <img v-else :src="require('@/assets/images/umbrella-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/setting.png')">
+            <img v-else :src="require('@/assets/images/setting-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/payment.png')">
+            <img v-else :src="require('@/assets/images/payment.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/card.png')">
+            <img v-else :src="require('@/assets/images/card-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/umbrella.png')">
+            <img v-else :src="require('@/assets/images/umbrella-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/setting.png')">
+            <img v-else :src="require('@/assets/images/setting-active.png')">
+          </div>
+          <div class="slider__item">
+            <img v-if="!active" :src="require('@/assets/images/payment.png')">
+            <img v-else :src="require('@/assets/images/payment.png')">
+          </div>
+        </vue-slick-carousel>
+        <button class="next-btn" @click="showNext"></button>
+      </div>
   </div>
 </template>
 
 <script>
-import CarouselItem from '@/components/CarouselItem.vue'
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
 export default {
   name: 'Carousel',
-  components: { CarouselItem },
-  props: {
-    imagesList: {
-      type: Array,
-      default: () => []
-    }
-  },
+  components: { VueSlickCarousel },
+  // props: {
+  //   imagesList: {
+  //     type: Array,
+  //     default: () => []
+  //   }
+  // },
   data() {
     return {
       currSliderIndex: 0
     }
   },
   methods: {
-    prevSlide() {
-      if (this.currSliderIndex > 0) {
-        this.currSliderIndex--
-      }
-
+    showNext() {
+      this.$refs.carousel.next()
     },
-    nextSlide() {
-      this.currSliderIndex++
-    }
+    showPrev() {
+      this.$refs.carousel.prev()
+    },
   }
 }
 </script>
@@ -44,8 +81,79 @@ export default {
 <style lang="scss">
 .carousel {
   display: flex;
-  justify-content: space-around;
-  width: 100%;
-  height: 100%;
+}
+.slider__item {
+  display: flex;
+  max-width: 300px;
+  height: 250px;
+  object-fit: contain;
+  overflow: hidden;
+  & img {
+    width: auto;
+    height: 100%;
+  }
+}
+.slider-wrapper {
+  display: flex;
+  align-items: center;
+}
+.slick-slide {
+  display: flex;
+}
+.carousel {
+  margin: auto 0;
+  width: 1400px;
+}
+.slick-slider[data-v-3d1a4f76] {
+  position: relative;
+  display: flex;
+  box-sizing: border-box;
+}
+.slick-list[data-v-3d1a4f76] {
+  position: relative;
+  display: block;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  transform: translateZ(0);
+}
+.carousel-sorce {
+  display: block;
+  justify-content: center;
+  align-items: center;
+}
+.slick-prev {
+  display: none;
+  border: none;
+  font-size: 0;
+}
+.slick-next {
+  display: none;
+  border: none;
+  font-size: 0;
+}
+.slick-dots {
+  li[class="slick-active"] {
+    button {
+      background-color: green;
+    }
+  }
+}
+.next-btn {
+  width: 104px;
+  height: 104px;
+  border-radius: 100%;
+  border: none;
+  background: url('../assets/images/next-btn.png');
+  background-size: contain;
+}
+.prev-btn {
+  display: flex;
+  width: 104px;
+  height: 104px;
+  border-radius: 100%;
+  border: none;
+  background: url('../assets/images/prev-btn.png');
+  background-size: contain;
 }
 </style>
--- src/components/credit/CreditParams.vue
+++ src/components/credit/CreditParams.vue
@@ -68,100 +68,40 @@
           </div>
         </div>
       </div>
-      <div class="slider-wrapper">
-        <vue-slick-carousel
-          :centerMode="true"
-          :arrows="true"
-          :infinite="true"
-          :slides-to-show="4"
-          :dots="false"
-          class="carousel"
-          :style="{'display': flex, 'width': '1600px', 'align-items': 'center'}"
-        >
-          <div v-for="image in giftsImages" :key="image.id" class="carousel-sorce">
-            <img :src="require('../../assets/images/'+image.img)">
-          </div>
-        </vue-slick-carousel>
-      </div>
-      <!-- <Carousel :imagesList="giftsImages" /> -->
+      <Carousel />
     </div>
   </div>
 </template>
 
 <script>
- import VueSlickCarousel from 'vue-slick-carousel'
- import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// import Carousel from '@/components/Carousel.vue'
+import Carousel from '@/components/Carousel.vue'
 export default {
   name: 'CreditParams',
-  components: { VueSlickCarousel},
+  components: { Carousel},
   data() {
     return {
+      active: true,
       years: 7,
       summ: '200000',
       showLoader: false,
       word: 'ЛЕТ',
-      giftsImages: [
-        {id: 1, name: 'sale', img: 'card.png'},
-        {id: 2, name: 'sale', img: 'umbrella.png'},
-        {id: 3, name: 'sale', img: 'setting.png'},
-        {id: 4, name: 'sale', img: 'payment.png'},
-        {id: 1, name: 'sale', img: 'card.png'},
-        {id: 2, name: 'sale', img: 'umbrella.png'},
-        {id: 3, name: 'sale', img: 'setting.png'},
-        {id: 4, name: 'sale', img: 'payment.png'},
-      ]
     }
   },
   methods: {
     checkYears() {
       this.word = (this.years == 1) ?  'ГОД' : (this.years == 0 || this.years >= 5) ? 'ЛЕТ' : 'ГОДА'
-    }
+    },
+    getIndex(index) {
+      console.log(index)
+    },
+    clickPrev(){
+      alert('Это я!!!')
+    },
   }
 }
 </script>
 
 <style lang="scss">
-.slider-wrapper {
-  display: flex;
-  width: 1600px;
-  height: 400px;
-}
-.slick-slide {
-  display: flex;
-}
-.carousel {
-  -webkit-appearance: none !important;
-  margin: auto 0;
-  width: 1600px;
-}
-.slick-slider[data-v-3d1a4f76] {
-  position: relative;
-  display: flex;
-  box-sizing: border-box;
-}
-.slick-track[data-v-e4caeaf8] {
-  display: flex;
-  align-items: center;
-}
-.carousel-sorce {
-  display: block;
-  justify-content: center;
-  align-items: center;
-}
-.slick-initialized .slick-slide[data-v-e4caeaf8] {
-  display: flex;
-  justify-content: center;
-}
-.slick-list[data-v-3d1a4f76] {
-  position: relative;
-  display: block;
-  overflow: hidden;
-  margin: 0;
-  padding: 0;
-  transform: translateZ(0);
-  width: 1600px;
-}
 .credit-params {
   display: flex;
   &__left {
