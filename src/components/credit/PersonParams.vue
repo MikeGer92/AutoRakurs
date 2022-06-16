f20a45b15cd31de57b411ee903b704fc6e86417c
unknown <mikeman@inbox.ru>
2022-06-16 08:29:08 +0300

    refacroring credit page

--- src/components/credit/PersonParams.vue
+++ src/components/credit/PersonParams.vue
@@ -8,16 +8,9 @@
       <div class="person__right_title">ЗАПОЛНИТЕ ПЕРСОНАЛЬНЫЕ ДАННЫЕ</div>
       <div class="person__right_main">
         <div class="person__right_main-left">
-          <div class="person__form">
-            <div class="person__form_title">Персональные данные</div>
-            <input type="text" class="person__form_name" placeholder="ФИО">
-            <input type="phone" class="person__form_phone" placeholder="+7 (___)  ___-__-__">
 
-            <CustomRadio />
+          <CallBackForm />
 
-            <button class="person__form_btn" type="button">Отправить заявку</button>
-            <p class="person__form_info">После отправки заявки вам перезвонит сотрудник банка,<br>проконсультирует и примет недостающие данные по телефону.</p>
-          </div>
         </div>
         <div class="person__right_main-right">
           <div class="person__howget">
@@ -44,10 +37,10 @@
 </template>
 
 <script>
-import CustomRadio from '@/components/CustomRadio.vue'
+import CallBackForm from '@/components/credit/CallBackForm.vue'
 export default {
   name: 'PersonParams',
-  components: { CustomRadio },
+  components: { CallBackForm },
   props: {
     blockNum: {
       type: String,
@@ -113,71 +106,6 @@ export default {
       }
     }
   }
-  &__form {
-    width: 750px;
-    height: 814px;
-    display: flex;
-    flex-direction: column;
-    background: #FFFFFF;
-    box-shadow: 0px 4.98225px 13.7012px rgba(0, 0, 0, 0.25);
-    border-radius: 48.5769px;
-    &_title {
-      display: flex;
-      margin: 61px 0 64px 64px;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 600;
-      font-size: 49.8225px;
-      line-height: 60px;
-      color: #000000;
-    }
-    &_name, &_phone {
-      display: flex;
-      width: 615px;
-      height: 90px;
-      padding: 0 0 0 30px;
-      margin: 0 0 22px 50px;
-      background: #F0F0F0;
-      border: none;
-      border-radius: 123.533px;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 36.3918px;
-      line-height: 44px;
-      color: #939393;
-    }
-    &_btn {
-      margin: 40px 0 0 50px;
-      width: 644px;
-      height: 100px;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 700;
-      font-size: 36.3918px;
-      line-height: 106.52%;
-      color: #FFFFFF;
-      border: none;
-      background: linear-gradient(90.43deg, #12609E 17.95%, #10205E 83.93%);
-      box-shadow: 0px 9.08375px 9.08375px rgba(0, 0, 0, 0.25);
-      border-radius: 84.0247px;
-    }
-    &_info {
-      margin: 44px 0 0 59px;
-      width: 607px;
-      height: 46px;
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 300;
-      font-size: 18.4658px;
-      line-height: 106.52%;
-      color: #000000;
-
-    }
-  }
   &__howget {
     width: 740px;
     height: 814px;
@@ -211,6 +139,7 @@ export default {
       text-decoration: none;
       & li {
         margin-bottom: 110px;
+        text-align: left;
         flex-direction: column;
         margin-left: 25px;
         font-family: 'Inter';
@@ -226,6 +155,7 @@ export default {
           list-style: none;
           & li {
             margin-bottom: 0;
+            text-align: left;
           }
         }
       }
