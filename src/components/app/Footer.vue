7ad81117411368d0b643e1ad6a5e92384e664c3a
unknown <mikeman@inbox.ru>
2022-08-01 10:19:02 +0300

    added Navbar & Footer 768px

--- src/components/app/Footer.vue
+++ src/components/app/Footer.vue
@@ -321,7 +321,72 @@ export default {
       &_right {
         max-width: 832px;
         &-title {
-          margin-left: 80px;
+          margin: 0 0 0 80px;
+          font-size: 18.0113px;
+          line-height: 22px;
+        }
+        &-list {
+          margin: 11px 0 27px 80px;
+          &--item {
+            width: 148px;
+            font-size: 9.78281px;
+            line-height: 12px;
+            margin: 11px 0;
+          }
+
+        }
+      }
+    }
+    &__bottom {
+      max-width: 1280px;
+      &_left {
+        max-width: 267px;
+        margin: 16px 0 40px 74px;
+        &-title {
+          font-size: 9.78135px;
+          line-height: 12px;
+        }
+        &-confid, &-confirm {
+          font-size: 9.78135px;
+          line-height: 12px;
+        }
+      }
+      &_right {
+        max-width: 636px;
+        margin: 28px 64px 0 ;
+        font-size: 9.78281px;
+        line-height: 12px;
+      }
+    }
+  }
+}
+@media (min-width: 768px) and (max-width: 1281px) {
+  .footer {
+    max-width: 1280px;
+    &__main {
+      &_left {
+        &-salon {
+          margin: 27px 0 0 72px;
+          max-width: 360px;
+          &--info, &--contact, &--credits {
+            &_title {
+              font-size: 18.0113px;
+              line-height: 22px;
+            }
+            & ul {
+              font-size: 9.78281px;
+              line-height: 12px;
+              & li {
+                margin: 15px 0;
+              }
+            }
+          }
+        }
+      }
+      &_right {
+        max-width: 832px;
+        &-title {
+          margin: 0 0 0 80px;
           font-size: 18.0113px;
           line-height: 22px;
         }
@@ -360,6 +425,76 @@ export default {
     }
   }
 
+}
+@media (min-width: 605px) and (max-width: 768px) {
+  .footer {
+    max-width: 768px;
+    &__main {
+      &_left {
+        &-salon {
+          margin: 17px 0 15px 44px;
+          max-width: 250px;
+          &--info, &--contact, &--credits {
+            &_title {
+              font-size: 10.9048px;
+              line-height: 13px;
+            }
+            & ul {
+              font-size: 5.92293px;
+              line-height: 7px;
+              & li {
+                margin: 9px 0;
+              }
+            }
+          }
+          &--contact {
+            &_title {
+              margin-top: 37px;
+            }
+          }
+        }
+      }
+      &_right {
+        max-width: 450px;
+        &-title {
+          margin: 17px 0 0 44px;
+          font-size: 10.9048px;
+          line-height: 13px;
+        }
+        &-list {
+          margin: 4.5px 0 0px 44px;
+          &--item {
+            width: 72px;
+            font-size: 5.92293px;
+            line-height: 7px;
+            margin: 4.5px 0;
+          }
+
+        }
+      }
+    }
+    &__bottom {
+      max-width: 768px;
+      &_left {
+        max-width: 161px;
+        margin: 10px 0 30px 44px;
+        &-title {
+          font-size: 5.92204px;
+          line-height: 7px;
+        }
+        &-confid, &-confirm {
+          font-size: 5.92204px;
+          line-height: 7px;
+        }
+      }
+      &_right {
+        max-width: 385px;
+        margin: 15px 44px 30px 0;
+        font-size: 5.92293px;
+        line-height: 7px;
+      }
+    }
+  }
 }
 @media (max-width: 605px) {
   .footer {
